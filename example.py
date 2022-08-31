8d0c320e02510a0c90d0ac387d20e10115b00768
kaboing <jonas@lincoln.se>
2022-08-31 11:00:06 +0200

    change in examples

--- example.py
+++ example.py
@@ -7,8 +7,8 @@ from aiogaro import Chargebox
 logging.basicConfig(level=logging.DEBUG)
 
 chargebox = Chargebox(
-        ipaddress.ip_address("10.0.1.207"), 
-        "2315250")
+        ipaddress.ip_address("10.0.1.27"), 
+        "1234567")
 
 loop = asyncio.get_event_loop()
 
